8242844823af1fdc1aa73eb2266c2bef0cb7d7c3
lmatusse <englmatusse@gmail.com>
2021-06-07 17:55:16 +0200

    subindo o projecto

--- src/main/java/mz/co/dsav1/model/Projecto.java
+++ src/main/java/mz/co/dsav1/model/Projecto.java
@@ -16,59 +16,77 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+
 @Entity
 public class Projecto {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@Column(nullable = false, length = 60)
 	private String designacao;
+	
 	@Column(nullable = false, length = 250)
 	private String descricao;
+	
 	@Column(nullable = false)
 	private Date prazo, data_entrada;
+	
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name="alocacao", joinColumns = {@JoinColumn(name="id_projeto")}, inverseJoinColumns = {@JoinColumn(name="id_cliente")})
+	@JoinTable(name = "alocacao", joinColumns = { @JoinColumn(name = "id_projeto") }, inverseJoinColumns = {
+	@JoinColumn(name = "id_cliente") })
 	private Collection<Cliente> clientes;
+	
 	@OneToMany(mappedBy = "projeto", fetch = FetchType.LAZY)
 	@Cascade(CascadeType.ALL)
 	private Collection<Modulo> modulo;
+
 	public Projecto(String designacao, String descricao, Date prazo, Date data_entrada) {
 		this.designacao = designacao;
 		this.descricao = descricao;
 		this.prazo = prazo;
 		this.data_entrada = data_entrada;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getDesignacao() {
 		return designacao;
 	}
+
 	public void setDesignacao(String designacao) {
 		this.designacao = designacao;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public Date getPrazo() {
 		return prazo;
 	}
+
 	public void setPrazo(Date prazo) {
 		this.prazo = prazo;
 	}
+
 	public Date getData_entrada() {
 		return data_entrada;
 	}
+
 	public void setData_entrada(Date data_entrada) {
 		this.data_entrada = data_entrada;
 	}
-	
 
 }
